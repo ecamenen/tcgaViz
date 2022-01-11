1c079824c1376a397dd39b2dd7f17dda4cc4fed7
ecamenen <etienne.camenen@gmail.com>
2022-01-11 16:14:44 +0100

    Clean comments, code reformat

--- tcga_viz.R
+++ tcga_viz.R
@@ -1,11 +1,14 @@
-# a simple tool to depict proportions of immune cells in any tumors
-#relative to the mean or median or quartiles of expression of any gene (or a signature?)
+# A visualization tool to depict proportions of immune cells in any tumors
+# relative to the average expression of any gene (or signature).
+
+########## Environment setting ##########
 
 # Input variables
 algorithm <- "Cibersort_ABS"
 disease <- "breast invasive carcinoma"
-geneX <- "ICOS"
+gene_x <- "ICOS"
 
+# Library loading
 library(openxlsx)
 library(dplyr)
 library(ggplot2)
@@ -17,52 +20,72 @@ library(readr)
 library(data.table)
 
 setwd("Documents/TCGA/")
-#Import Cybersort dataset (to be modified for various algorithms: sheet)
-#ideally one would want to be able to select which algorithms to use before import
-TCGA_pop <- read.xlsx("TCGA.xlsx", sheet = algorithm)
-#ideally one would want to be able to select tumor type before selecting the gene
-Tumor_type <- read_delim('TCGA_phenotype_denseDataOnlyDownload.tsv')
-if (!is.null(disease))
-    Tumor_type <- subset(
-        Tumor_type,
+
+########## Dataset loading ##########
+
+# Import the phenotypes file
+tcga_pop <- read.xlsx("TCGA.xlsx", sheet = algorithm)
+
+# Import the tumor type file
+tumor_type <- read_delim("TCGA_phenotype_denseDataOnlyDownload.tsv")
+if (!is.null(disease)) {
+    tumor_type <- subset(
+        tumor_type,
         subset = `_primary_disease` == disease,
-        select = "sample")
+        select = "sample"
+    )
+}
+
 # Double the buffer size
 Sys.setenv(VROOM_CONNECTION_SIZE = 131072 * 2)
-#Import gene dataset (to be modified: pathway)
-Gene <- read_tsv("EB++AdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz")
-Gene <- transpose(Gene, keep.names = "col", make.names = "sample")
+# Import the gene file
+gene <- read_tsv("EB++AdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz")
+gene <- transpose(gene, keep.names = "col", make.names = "sample")
 
-#Merge datasets + remove sample with NA
-Dataset <- merge(Tumor_type, Gene, by = 1)
-Dataset <- merge(TCGA_pop, Dataset, by = 1)
+# Merge datasets
+dataset <- merge(tumor_type, gene, by = 1)
+dataset <- merge(tcga_pop, dataset, by = 1)
 
-#Calcul cuttedian of Gene + creation column > cutoff (ideally one would want to be able to filter on median
-#and quartiles as well. Would be even better if one could enter a list of genes: the cut off would then be
-#on the mean value for all genes listed)
-cutoff <- mean(as.numeric(Dataset[, geneX]), na.rm = TRUE)
-#number of patients in each category should be indicated in the figure
+########## Data formatting ##########
 
-cutted <- mutate(Dataset,high=Dataset[, geneX]>cutoff)
+# Calculation of the gene_x expression median
+cutoff <- mean(as.numeric(dataset[, gene_x]), na.rm = TRUE)
 
-#Plot sous type cell selon niveau d'expression du gène + stats
-cutted_melt <- subset(cutted, select = c(colnames(TCGA_pop)[-seq(2)], "high"))
-sub_cutted_melt  <- melt(cutted_melt)
+# Add a column for a higher level than gene_x
+cutted <- mutate(dataset, high = dataset[, gene_x] > cutoff)
+
+# Remove the samples, study and the gene columns
+cutted_melt <- subset(cutted, select = c(colnames(tcga_pop)[-seq(2)], "high"))
+
+# Get a 3-column table with the value associated with each subtype and its level
+sub_cutted_melt <- melt(cutted_melt)
 colnames(sub_cutted_melt)[2] <- "cell_type"
-sub_cutted_melt$value<-sub_cutted_melt$value+1e-5
-pop <- ggviolin(sub_cutted_melt,x="high", y="value", facet.by = "cell_type", outlier.shape = NA,
-                 color = "cell_type")+
-  scale_x_discrete(name=paste(geneX, "level"), labels=c("FALSE"="Low","TRUE"="High")) +
-  scale_y_log10()
-pop  <- ggpar(pop, legend = 'none')
+sub_cutted_melt$value <- sub_cutted_melt$value + 1e-05
 
-stat3 <- sub_cutted_melt%>%
-  group_by(cell_type)%>%
-  wilcox_test(value~high)%>%
-  adjust_pvalue(method="BH")%>%
-  add_significance() %>%
-  subset(p.adj < 0.1)%>%
-  add_xy_position(x="high")
-pop <- pop + stat_pvalue_manual(stat3, label = "p.adj.signif")
-pop
+########## Plot ##########
 
+# Plot the cell subtypes according to the gene expression level
+pop <- ggviolin(
+    data = sub_cutted_melt,
+    x = "high",
+    y = "value",
+    facet.by = "cell_type",
+    outlier.shape = NA,
+    color = "cell_type"
+) +
+    scale_x_discrete(
+        name = paste(gene_x, "level"),
+        labels = c(`FALSE` = "Low", `TRUE` = "High")
+    ) +
+    scale_y_log10()
+pop <- ggpar(pop, legend = "none")
+
+# Add corrected Wilcoxon tests
+stat <- sub_cutted_melt %>%
+    group_by(cell_type) %>%
+    wilcox_test(value ~ high) %>%
+    adjust_pvalue(method = "BH") %>%
+    add_significance() %>%
+    subset(p.adj < 0.1) %>%
+    add_xy_position(x = "high")
+pop + stat_pvalue_manual(stat, label = "p.adj.signif")
