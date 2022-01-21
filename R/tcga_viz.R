9d6152c32601eef3a58d9a3710dbfd33f25b5306
ecamenen <etienne.camenen@gmail.com>
2022-01-21 22:40:42 +0100

    Add check.yaml

--- R/tcga_viz.R
+++ R/tcga_viz.R
@@ -3,23 +3,21 @@
 
 ########## Environment setting ##########
 
-# Input variables
 algorithm <- "Cibersort_ABS"
 disease <- "breast invasive carcinoma"
 gene_x <- "A"
-gene_file <- "tcga_genes.tsv"
-cell_file <- "cell_pop.xlsx"
-phenotype_file <- "tcga_phenotypes.tsv"
-
-setwd(Sys.glob(file.path("*", "extdata")))
+gene_file <- "/tcga_genes.tsv"
+cell_file <- "/cell_pop.xlsx"
+phenotype_file <- "/tcga_phenotypes.tsv"
+path <- file.path("inst", "extdata")
 
 ########## Dataset loading ##########
 
 # Import the phenotypes file
-tcga_pop <- read.xlsx(cell_file, sheet = algorithm)
+tcga_pop <- read.xlsx(paste0(path, cell_file), sheet = algorithm)
 
 # Import the tumor type file
-tumor_type <- read_delim(phenotype_file, show_col_types = FALSE)
+tumor_type <- read_delim(paste0(path, phenotype_file), show_col_types = FALSE)
 if (!is.null(disease)) {
     tumor_type <- subset(
         tumor_type,
@@ -31,17 +29,14 @@ if (!is.null(disease)) {
 # Double the buffer size
 Sys.setenv(VROOM_CONNECTION_SIZE = 131072 * 2)
 # Import the gene file
-gene <- read_delim(gene_file, show_col_types = FALSE)
+gene <- read_delim(paste0(path, gene_file), show_col_types = FALSE)
 gene <- transpose(gene, keep.names = "col", make.names = "sample")
 
 # Merge datasets
 dataset <- merge(tumor_type, gene, by = 1)
 
-########## Data formatting ##########
-
+# Data formatting
 sub_cutted_melt <- convert_biodata(dataset, tcga_pop, gene_x)
-
-########## Plot ##########
 
 # Plot the cell subtypes according to the gene expression level
 p <- plot_violin(sub_cutted_melt, gene_x)
