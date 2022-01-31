0bb6cadd1467d260878061ff35af9a0fddc43a54
ecamenen <etienne.camenen@gmail.com>
2022-01-31 17:58:05 +0100

    Update data-test

--- dev/create_data.R
+++ dev/create_data.R
@@ -1,13 +1,13 @@
-n <- 10
+n <- 30
 p <- 5
-i_sign <- sample(seq(n), 3)
+i_sign <- round(n * 1/4)
 
 val_sign <- sapply(
-    seq(length(i_sign)),
+    seq(i_sign),
     function(x) c(runif(p, max = 10), runif(p, min = .1, max = .25))
 )
 val_n_sign <- sapply(
-    seq(n - length(i_sign)),
+    seq(n - i_sign),
     function(x) c(rep(0, p), runif(p, max = .05))
 )
 tab <- cbind(val_sign, val_n_sign)
@@ -21,13 +21,14 @@ genes <- data.frame(sample = LETTERS[seq(p)], tab[seq(p), ])
 phenotypes <- data.frame(sample, 1, "Primary Tumor", "breast invasive carcinoma")
 colnames(phenotypes)[2:4] <- c("sample_type_id", "sample_type", "_primary_disease")
 
-path <- "inst/extdata/"
+path <- file.path("inst", "extdata")
 openxlsx::write.xlsx(
     cells,
-    paste0(path, "cell_pop.xlsx", sheetName = "CibersorABS")
+    file.path(path, "cell_pop.xlsx"),
+    sheetName = "Cibersort_ABS"
 )
-readr::write_tsv(genes, paste0(path, "tcga_genes.tsv"))
-readr::write_tsv(phenotypes, paste0(path, "tcga_phenotypes.tsv"))
+readr::write_tsv(genes, file.path(path, "tcga_genes.tsv"))
+readr::write_tsv(phenotypes, file.path(path, "tcga_phenotypes.tsv"))
 
 genes_temp <- data.table::transpose(genes, keep.names = "sample")
 colnames(genes_temp) <- genes_temp[1, ]
