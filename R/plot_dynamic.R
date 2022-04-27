49a5aa37593f7ca1fa875e67f7b96c5136353985
ecamenen <etienne.camenen@gmail.com>
2022-04-27 17:12:06 +0200

    Add margin to plotly

--- R/plot_dynamic.R
+++ R/plot_dynamic.R
@@ -1,9 +1,9 @@
 plot_dynamic <- function(p) {
-    ggplotly(p) %>%
+    p2 <- ggplotly(p) %>%
         plotly::config(
             editable = TRUE,
             displaylogo = FALSE,
-            edits = list(shapePosition = F),
+            edits = list(shapePosition = FALSE),
             modeBarButtons = list(list("toImage")),
             toImageButtonOptions = list(
                 filename = "tcga_plot.png",
@@ -11,5 +11,8 @@ plot_dynamic <- function(p) {
                 width = 1000,
                 height = 1000
             )
-        )
+        ) %>%
+        layout(margin = list(t = 100, r = 100))
+    p2$x$layout$annotations[[2]]$x <- -0.075
+    return(p2)
 }
