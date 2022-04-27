85fa8b744aa2e76f876c8fa5f3dfe42f3751a287
ecamenen <etienne.camenen@gmail.com>
2022-04-27 14:41:25 +0200

    Add method_test and method_adjust parameters to calculate_pvalue

--- R/calculate_pvalue.R
+++ R/calculate_pvalue.R
@@ -1,10 +1,14 @@
 #' Corrected Wilcoxon tests
 #'
 #' Displays stars for each cell type corresponding to the significance level
-#' of Wilcoxon tests between expression levels (high or low) with
-#' Benjamini-Hochberg multiple correction.
+#' of two mean comparison tests between expression levels (high or low) with
+#' multiple correction.
 #'
 #' @inheritParams plot_violin
+#' @param method_test character for the choice of the statistical test among
+#' 't_test' or 'wilcox_test'.
+#' @param method_adjust character for the choice of the multiple correction test
+#' among `r paste0("'", paste0(sort(p.adjust.methods), collapse = "', '"), "'")`
 #' @param p_threshold float for the significativity threshold of the P-value.
 #'
 #' @return rstatix_test object for a table with cell types in the row and
@@ -14,13 +18,31 @@
 #' @examples
 #' data(tcga)
 #' df <- convert_biodata(tcga$genes, tcga$cells, "A")
-#' stats <- calculate_pvalue(df)
-calculate_pvalue <- function(data, p_threshold = 0.05) {
+#' calculate_pvalue(df)
+#' calculate_pvalue(df, method_test = "t_test", method_adjust = "bonferroni")
+calculate_pvalue <- function(
+    data,
+    method_test = "wilcox_test",
+    method_adjust = "BH",
+    p_threshold = 0.05
+) {
+    if (!method_test %in% c("t_test", "wilcox_test")) {
+        stop("Please select an option between 't_test' or 'wilcox_test'")
+    }
+    if (!method_adjust %in% p.adjust.methods) {
+        stop(
+            paste0(
+                "Please select an option in '",
+                paste0(sort(p.adjust.methods), collapse = "', '"),
+                "'"
+            )
+        )
+    }
     data %>%
         remove_similar_type_levels() %>%
         group_by(cell_type) %>%
-        wilcox_test(value ~ high) %>%
-        adjust_pvalue(method = "BH") %>%
+        base::get(method_test)(value ~ high) %>%
+        adjust_pvalue(method = method_adjust) %>%
         add_significance() %>%
         subset(p.adj < p_threshold) %>%
         add_xy_position(x = "high")
