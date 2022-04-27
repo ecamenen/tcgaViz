8f10110c13511e13d35a58b7dc1d10095e99162c
ecamenen <etienne.camenen@gmail.com>
2022-04-27 15:03:10 +0200

    Include biodata and biostats objects and  pvalue_manual to plot_violin
    Update doc and xlab for plot_violin

--- R/plot_violin.R
+++ R/plot_violin.R
@@ -3,26 +3,39 @@
 #' Violin plot of cell subtypes according to the expression level (high or
 #' low) of a selected gene.
 #'
-#' @param data dataframe containing columns named high (logical),
-#' cell_type (factor) and value (float).
+#' @param data object from [convert2biodata()] for a dataframe containing
+#' columns named high (logical), cell_type (factor) and value (float).
 #' @param type character for the type of plot to be chosen among "violin"
 #' or "boxplot".
 #' @param dots boolean to add all data to the graph as points.
 #' @param title character for the title of the plot.
 #' @param xlab character for the name of the X axis label.
 #' @param ylab character for the name of the X axis label.
+#' @param stats object from [calculate_pvalue()].
 #' @param ... arguments to pass to [ggplot2::theme()].
 #'
 #' @return ggrub object for a violon plot.
 #' @export
 #'
 #' @examples
-#' library(ggpubr)
+#' library("ggplot2")
 #' data(tcga)
 #' df <- convert_biodata(tcga$genes, tcga$cells, "A")
-#' p <- plot_violin(df)
+#' plot_violin(df)
 #' stats <- calculate_pvalue(df)
-#' p + stat_pvalue_manual(stats, label = "p.adj.signif")
+#' plot_violin(
+#'     df,
+#'     type = "boxplot",
+#'     dots = TRUE,
+#'     xlab = "Level of cell type differentiation from the Cibersort algorithm
+#'     based on the 'A' gene expression.",
+#'     ylab = "Percent of relative abundance",
+#'     title = "Random distribution from TCGA database",
+#'     stats = stats,
+#'     axis.text.y = element_text(size = 8),
+#'     axis.title = element_text(face = "bold.italic"),
+#'     plot.title =  element_text(face = "bold", hjust = 0.5)
+#' )
 plot_violin <- function(
     data,
     type = "violin",
@@ -30,10 +43,15 @@ plot_violin <- function(
     title = NULL,
     xlab = NULL,
     ylab = NULL,
+    stats = NULL,
     ...
 ) {
     if (!type %in% c("violin", "boxplot")) {
         stop("Please select an option between 'violin' or 'boxplot'")
+    }
+    stopifnot(is(data, "biodata"))
+    if (!is.null(stats)) {
+        stopifnot(is(stats, "biostats"))
     }
 
     func <- quote(base::get(paste0("gg", type))(
@@ -47,6 +65,13 @@ plot_violin <- function(
 
     if (type == "violin") {
         func$draw_quantiles <- c(0.25, 0.5, 0.75)
+    }
+    if (is.null(xlab)) {
+        xlab <- paste(
+            "Level of cell type differentiation based on the",
+            attributes(data)$gene,
+            "gene expression."
+        )
     }
     if (is.null(ylab)) {
         ylab <- ""
@@ -73,5 +98,11 @@ plot_violin <- function(
         )
     }
 
-    ggpar(pop, legend = "none")
+    p <- ggpar(pop, legend = "none")
+
+    if (!is.null(stats)) {
+        p <- p + ggpubr::stat_pvalue_manual(stats, label = "p.adj.signif")
+    }
+
+    return(p)
 }
