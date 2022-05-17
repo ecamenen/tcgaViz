d0019a83b768f395e7634b5b70e2692100ed4935
ecamenen <etienne.camenen@gmail.com>
2022-05-17 17:28:20 +0200

    Format the ui text, update the app

--- R/app_utils.R
+++ R/app_utils.R
@@ -8,3 +8,12 @@ show_notif <- function(f, m) {
     on.exit(removeNotification(id), add = TRUE)
     f
 }
+
+format_correction_choices <- function() {
+    names <- sort(p.adjust.methods)[-4]
+    correction_choices <- as.list(names)
+    names(correction_choices) <- str_to_title(names)
+    names(correction_choices)[1] <- "Benjamini & Hochberg"
+    names(correction_choices)[3] <- "Benjamini & Yekutieli"
+    return(correction_choices)
+}
