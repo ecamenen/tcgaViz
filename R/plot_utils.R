f10d19b89367a17cdded8d8396c1e4a6d1f9b0b0
ecamenen <etienne.camenen@gmail.com>
2022-05-17 15:41:30 +0200

    Add parameters for plot.biodata

--- R/plot_utils.R
+++ R/plot_utils.R
@@ -7,14 +7,43 @@ plot.biodata <- function(
     xlab = NULL,
     ylab = NULL,
     stats = NULL,
+    draw = TRUE,
     axis.text.x = element_text(size = 10),
     axis.text.y = element_text(size = 8),
-    axis.title.x = element_text(size = 12, face = "bold.italic", vjust = -0.5),
-    axis.title.y = element_text(size = 12, face = "bold.italic", vjust = -0.5),
-    plot.title =  element_text(size = 16, face = "bold", vjust = 1, hjust = 0.5),
+    cex.lab = 12,
+    cex.main = 16,
+    col = scales::hue_pal()(length(unique(x$cell_type))),
+    axis.title.x = element_text(
+        size = cex.lab,
+        face = "bold.italic",
+        vjust = -0.5
+    ),
+    axis.title.y = element_text(
+        size = cex.lab,
+        face = "bold.italic",
+        vjust = -0.5
+    ),
+    plot.title = element_text(
+        size = cex.main,
+        face = "bold",
+        vjust = 1,
+        hjust = 0.5
+    ),
+    plot.margin = unit(c(0, 0, 0, -0.5), "cm"),
     ...
 ) {
-    plot_violin(
+    n <- length(unique(x$cell_type))
+    if (length(col) != n) {
+        stop(
+            paste0(
+                "the length of the `col` parameter vector must be the same",
+                "size as the number of cell types, i.e. ",
+                n,
+                "."
+            )
+        )
+    }
+    p <- plot_distribution(
         x,
         type = type,
         dots = dots,
@@ -28,5 +57,14 @@ plot.biodata <- function(
         axis.title.y = axis.title.y,
         plot.title =  plot.title,
         ...
+    ) + scale_color_manual(
+        values = col
     )
+    if (draw) {
+        options(warn = -1)
+        plot(p)
+        options(warn = 0)
+    } else {
+        return(p)
+    }
 }
