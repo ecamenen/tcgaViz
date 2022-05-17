e5ffd47a526f0eded58d1ad183c4f2e7d881979a
ecamenen <etienne.camenen@gmail.com>
2022-05-17 15:48:49 +0200

    Refactoring of the cli

--- inst/tcga_viz.R
+++ inst/tcga_viz.R
@@ -10,48 +10,19 @@ tissue <- "Primary Tumor"
 path <- file.path(golem::get_golem_wd(), "inst", "extdata")
 
 ########## Dataset loading ##########
-load(file.path(path, "tcga_raw.rda"))
 message("TCGA loading in progess...")
-
-# Import the phenotypes file
-tcga_pop <- tcga_raw$cells[[algorithm]]
-
-# Import the tumor type file
-diseases <- tcga_raw$phenotype
-if (!is.null(disease)) {
-    diseases <- diseases[
-        diseases$`_primary_disease` == disease,
-    ]
-}
-
-tumor_type <- diseases
-if (!is.null(disease)) {
-    tumor_type <- diseases[
-        diseases$sample_type == tissue,
-        "sample"
-    ]
-    if (nrow(tumor_type) == 0)
-        stop("Selected tissue has 0 element.")
-}
-
-# Import the gene file
-gene <- dplyr::select(
-    tcga_raw$genes,
-    col,
-    all_of(gene_x)
-)
-
-# Merge datasets
-dataset <- merge(tumor_type, gene, by = 1)
+load(file.path(path, "tcga.rda"))
 
 # Data formatting
-sub_cutted_melt <- convert_biodata(dataset, tcga_pop, gene_x)
+df <- convert2biodata(algorithm, disease, tissue, gene_x, stat = "mean")
+
+# Add corrected tests
+stats <- calculate_pvalue(
+    df,
+    method_test = "wilcox_test",
+    method_adjust = "BH",
+    p_threshold = 0.05
+)
 
 # Plot the cell subtypes according to the gene expression level
-p <- plot_violin(sub_cutted_melt, gene_x)
-
-# Add corrected Wilcoxon tests
-stat <- calculate_pvalue(sub_cutted_melt)
-options(warn = -1)
-p + ggpubr::stat_pvalue_manual(stat, label = "p.adj.signif")
-options(warn = 0)
+plot(df, stats = stats, type = "violin", dots = FALSE)
